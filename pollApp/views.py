89e8e79a02fd45fb836a17820172cae8a194591d
Benedict <newlyf38@gmail.com>
2023-03-01 12:26:13 +0000

    recreated the detail view

--- pollApp/views.py
+++ pollApp/views.py
@@ -11,7 +11,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question Does not Exist')
+    return render(request, 'pollApp/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
