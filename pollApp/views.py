a7c4ff51526f070e69cc48bf8b99418b6f20713b
Benedict <newlyf38@gmail.com>
2023-03-01 12:16:57 +0000

    used the  render method to load my templates and passes my context dictionary

--- pollApp/views.py
+++ pollApp/views.py
@@ -6,11 +6,8 @@ from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('pollApp/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
