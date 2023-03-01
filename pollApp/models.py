d0771e5948afb73a43263d7f15b82d3a2119d0c8
Benedict <newlyf38@gmail.com>
2023-03-01 11:59:52 +0000

    added a string function to the Choice model to display the name of the choice anytime it gets created

--- pollApp/models.py
+++ pollApp/models.py
@@ -13,3 +13,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.question.question_text
