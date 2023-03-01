519e367dee6833341e72ba4bbeeec3746484dea4
Benedict <newlyf38@gmail.com>
2023-03-01 12:02:10 +0000

    imported timezone and datetime in my model

--- pollApp/models.py
+++ pollApp/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    
     
     def __str__(self):
         return self.question_text
@@ -16,3 +19,5 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.question.question_text
+    
+    
