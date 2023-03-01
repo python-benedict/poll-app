187358e7531576c8a31ca5cb6e91929fe40287d5
Benedict <newlyf38@gmail.com>
2023-03-01 11:25:09 +0000

    added a vote view to the project

--- pollApp/views.py
+++ pollApp/views.py
@@ -12,4 +12,5 @@ def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
