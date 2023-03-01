b58d138f80ade0469a407bd587b774e2f4a42eb9
Benedict <newlyf38@gmail.com>
2023-03-01 11:24:16 +0000

    added a detail view to the project

--- pollApp/views.py
+++ pollApp/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello and welcome to our page')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
