c27dee69cd0a923e75c2256b1b7fee20077bc3f0
Benedict <newlyf38@gmail.com>
2023-03-01 12:21:29 +0000

    added an Http404 to the project

--- pollApp/views.py
+++ pollApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question, Choice
 from django.template import loader
+from django.http import Http404
 # Create your views here.
 
 def index(request):
