a249da4d3b191ec3be81e74f16fd204d8f5d2628
Benedict <newlyf38@gmail.com>
2023-03-01 11:44:52 +0000

    created a directory called templates/pollApp/index.html

--- pollApp/views.py
+++ pollApp/views.py
@@ -4,7 +4,9 @@ from .models import Question, Choice
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello and welcome to our page')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
