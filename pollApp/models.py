5732c6f1e2b09811e1db6aa142feca4304d0d306
Benedict <newlyf38@gmail.com>
2023-03-01 12:03:23 +0000

    created a method calle was_published_at to check whether the day published was recently

--- pollApp/models.py
+++ pollApp/models.py
@@ -7,7 +7,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
-    
+    def was_published_at(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     def __str__(self):
         return self.question_text
