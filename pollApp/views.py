f20c8414b1eb459295cf401d287b90dc25ca9d84
Benedict <newlyf38@gmail.com>
2023-03-05 15:44:52 +0000

    commented the index, and detail view

--- pollApp/views.py
+++ pollApp/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from .models import Question, Choice
-from django.template import loader
-from django.http import Http404
-# Create your views here.
+# from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.urls import reverse
+# from .models import Question, Choice
+# from django.template import loader
+# from django.http import Http404
+# # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'pollApp/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'pollApp/index.html', context)
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(id=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question Does not Exist')
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollApp/detail.html', {'question':question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(id=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('Question Does not Exist')
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'pollApp/detail.html', {'question':question})
+
+
+
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
