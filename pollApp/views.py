1671138712db0908db7f7ab2aeb8d0c71450c641
Benedict <newlyf38@gmail.com>
2023-03-05 15:47:45 +0000

    removed the redundant functional based view from the project

--- pollApp/views.py
+++ pollApp/views.py
@@ -50,8 +50,6 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def vote(request, question_id):
-    ... # same as above, no changes needed.
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
