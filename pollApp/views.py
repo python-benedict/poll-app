d678700d5ae5f2c1b2795110aaa982998f667aab
Benedict <newlyf38@gmail.com>
2023-03-05 15:00:14 +0000

    added a question view to the project

--- pollApp/views.py
+++ pollApp/views.py
@@ -20,8 +20,8 @@ def detail(request, question_id):
     return render(request, 'pollApp/detail.html', {'question':question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pollApp/result.html', {'question':question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
