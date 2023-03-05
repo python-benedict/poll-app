2dc64fdd1bf88025dae40b69bfa887d1bcc072b5
Benedict <newlyf38@gmail.com>
2023-03-05 14:06:42 +0000

    imported reverse from urls

--- pollApp/views.py
+++ pollApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
