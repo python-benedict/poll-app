08a57a42968a0289f48a25e30f7c9bec559a5422
Benedict <newlyf38@gmail.com>
2023-03-08 18:07:12 +0000

    added a test to chech whether test was published recently with old questions

--- pollApp/tests.py
+++ pollApp/tests.py
@@ -15,3 +15,13 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+        
+    
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
