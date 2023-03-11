6c28c955d0598a21b20f5e2be654687622e0b0e1
Benedict <newlyf38@gmail.com>
2023-03-11 09:53:45 +0000

    imported timezone in the views purposely to make changes in hte indexview class

--- pollApp/views.py
+++ pollApp/views.py
@@ -27,6 +27,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
