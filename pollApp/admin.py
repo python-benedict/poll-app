d84f0481467738f47962854a345ef751679e7b33
Benedict <newlyf38@gmail.com>
2023-03-12 09:32:58 +0000

    changes the ChoiceInline(admin.StackedInline) to ChoiceInline(admin.TabularInline)

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
