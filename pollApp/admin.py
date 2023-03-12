ab2ad9b3b2d2dace17a760a63cefb89c0e35a1b3
Benedict <newlyf38@gmail.com>
2023-03-12 08:56:58 +0000

    added a QuestionAdmin class to the project

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
 admin.site.register(Question)
 admin.site.register(Choice)
