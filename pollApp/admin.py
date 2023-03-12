1dfebc60983e455439b80b0bb95b2e93aa235a7e
Benedict <newlyf38@gmail.com>
2023-03-12 09:31:22 +0000

    added a ChoiceInline class to display choices in the question page

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -2,21 +2,26 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = (
         ('Published Date Information', {
             "fields": (
                 ['pub_date']
-            ),
+            ), 'classes':['collapse']
         }),
         ('Question Information', {
             "fields": (
                 ['question_text']
-            ),
+            ), 'classes':['collapse']
         }),
     )
     
+    inlines = [ChoiceInline]
     
 
 admin.site.register(Question, QuestionAdmin)
