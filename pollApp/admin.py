409d50835649716e88e1d9e1fb8e7896ce249d64
Benedict <newlyf38@gmail.com>
2023-03-12 08:57:59 +0000

    registerd the QuestionAdmin

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -5,5 +5,5 @@ from .models import Question, Choice
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
