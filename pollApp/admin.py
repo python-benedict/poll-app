a39a3ad93e83ca6309aef2e5d910ae5e63a48ab0
Benedict <newlyf38@gmail.com>
2023-03-12 09:55:56 +0000

    added a list fileter to the project

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -24,6 +24,7 @@ class QuestionAdmin(admin.ModelAdmin):
     
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
     
 
 admin.site.register(Question, QuestionAdmin)
