9a3866fb2f18b2bd9783c6eda9358fd9bff050cf
Benedict <newlyf38@gmail.com>
2023-03-12 09:07:19 +0000

    added a fieldsets to the question admin

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -3,7 +3,21 @@ from .models import Question, Choice
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    # fields = ['pub_date', 'question_text']
+    fieldsets = (
+        ('Published Date Information', {
+            "fields": (
+                ['pub_date']
+            ),
+        }),
+        ('Question Information', {
+            "fields": (
+                ['question_text']
+            ),
+        }),
+    )
+    
+    
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
