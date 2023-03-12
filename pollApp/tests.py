64ff76484af4963dd4839746642a8808c36d39d9
Benedict <newlyf38@gmail.com>
2023-03-12 07:25:30 +0000

    wrote a test for two past questions

--- pollApp/tests.py
+++ pollApp/tests.py
@@ -70,3 +70,38 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'No polls are available')
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+
+    # def test_two_past_questions(self):
+    #         """
+    #         The questions index page may display multiple questions.
+    #         """
+    #         question1 = create_question(question_text="Past question 1.", days=-30)
+    #         question2 = create_question(question_text="Past question 2.", days=-5)
+    #         response = self.client.get(reverse('pollApp:index'))
+            # self.assertQuerysetEqual(
+            #     response.context['latest_question_list'],
+            #     [question2, question1],
+            # )
+            
+
+    # def test_future_question_and_past_question(self):
+    #     """
+    #     Even if both past and future questions exist, only past questions
+    #     are displayed.
+    #     """
+    #     question = create_question(question_text="Past question.", days=-30)
+    #     create_question(question_text="Future question.", days=30)
+    #     response = self.client.get(reverse('polls:index'))
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_question_list'],
+    #         [question],
+    #     )
+    
+    def test_for_two_past_questions(self):
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1],)
+        
